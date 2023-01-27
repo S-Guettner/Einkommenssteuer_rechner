e5ad5f983f08067a21cd46241787c044026f0062
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-27 11:17:35 +0100

    var to change to input value

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,20 +15,22 @@ const output = document.getElementById("output")
 
 
 /* ============ variables global ============ */
-const freiBetrag2020 = 9408
-const freiBetrag2021 = 9744
-const freiBetrag2022 = 10347
+
 
 let steuerGrenze1
 let steuerGrenze2
 let steuerGrenze3
-let steuerGrenze4
+
 
 const calculate = () =>{
     let einkommen
+    let freiBetrag
     splittingtarif.checked ? einkommen = Number(einkommenInput.value) / 2 : einkommen = Number(einkommenInput.value)
     console.log(einkommen);
 
-    zweitausenzwanzig.checked ? 
+    zweitausenzwanzig.checked ? (freiBetrag = 9408,steuerGrenze1 = 14532, steuerGrenze2 = 57051, steuerGrenze3 = 270500) : zweitausendeinundzwanzig.checked ? (freiBetrag = 9744, steuerGrenze1 = 14753, steuerGrenze2 = 57918, steuerGrenze3 = 274612) : (freiBetrag = 10347, steuerGrenze1 = 14926, steuerGrenze2 = 58596, steuerGrenze3 = 277825)
+
+
+    console.log(freiBetrag);
 }
 
