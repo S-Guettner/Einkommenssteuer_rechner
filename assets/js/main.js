14c81be92d21f8a0c947e14902c09a578d351322
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-27 11:43:11 +0100

    calculated

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,12 +25,17 @@ let steuerGrenze3
 const calculate = () =>{
     let einkommen
     let freiBetrag
+    let einkommenssteuer
     splittingtarif.checked ? einkommen = Number(einkommenInput.value) / 2 : einkommen = Number(einkommenInput.value)
     console.log(einkommen);
 
     zweitausenzwanzig.checked ? (freiBetrag = 9408,steuerGrenze1 = 14532, steuerGrenze2 = 57051, steuerGrenze3 = 270500) : zweitausendeinundzwanzig.checked ? (freiBetrag = 9744, steuerGrenze1 = 14753, steuerGrenze2 = 57918, steuerGrenze3 = 274612) : (freiBetrag = 10347, steuerGrenze1 = 14926, steuerGrenze2 = 58596, steuerGrenze3 = 277825)
 
-
-    console.log(freiBetrag);
+    if(einkommen <= freiBetrag){
+        return einkommenssteuer = 0
+    }
+    else if(einkommen > freiBetrag && einkommen <= steuerGrenze1){
+        let y = (einkommen - freiBetrag) / 10000
+        einkommenssteuer = 
+    }
 }
-
