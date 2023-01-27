d0762f54d61619e5073e16d60050dbba2a1ee47a
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-27 10:39:37 +0100

    js

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+/* ============ input ================ */
+/* tarif */
+const grundtarif = document.getElementById("grundtarif")
+const splittingtarif = document.getElementById("splittingtarif")
+/* steuerjahr */
+const zweitausenzwanzig = document.getElementById("zweitausenzwanzig")
+const zweitausendeinundzwanzig = document.getElementById("zweitausendeinundzwanzig")
+const zweitausendzweiundzwanzig = document.getElementById("zweitausendzweiundzwanzig")
+/* einkommen */
+const einkommenInput = document.getElementById("einkommenInput")
+/* submit */
+const button = document.getElementById("button")
+/* ========== output ====================== */
+const output = document.getElementById("output")
+
+
+/* ============ variables global ============ */
+const freiBetrag2020 = 9408
+const freiBetrag2021 = 9744
+const freiBetrag2022 = 10347
+
+button.addEventListener('click', () =>{
+    let einkommen
+    splittingtarif.checked ? einkommen = Number(einkommenInput.value) / 2 : einkommen = Number(einkommenInput.value)
+    console.log(einkommen);
+
+    
+})
+
+/* const tarif = () =>{
+    let einkommen = parseInt(einkommenInput.value);
+    splittingtarif.checked ? einkommen/2 : einkommen * 1; 
+    console.log(einkommen);
+} */
+
+
+/* 
+const calculate = () =>{
+    
+} */
